178f78c9a614c32c99e2284aa45f5262083df585
hetvi <hvkavathiya123@gmail.com>
2024-01-19 10:34:57 +0530

    login system

--- application/views/layout/script.php
+++ application/views/layout/script.php
@@ -6,7 +6,9 @@
   <script src="<?=base_url("assets")?>/dist/js/tabler.min.js?1684106062" defer></script>
   <script src="<?=base_url("assets")?>/dist/js/demo.min.js?1684106062" defer></script>
   
-  <script class="appendHere"></script>
+  <script class="appendHere">
+    
+  </script>
   <script>
       $(document).ready(function() {
           const perPageJs = $('.javascript').html();
