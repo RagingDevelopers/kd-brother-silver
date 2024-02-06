c9bb27ad065dadf2ea6ce2f0991095b38ef17b81
manan-patel1998 <gogarimanan12@gmail.com>
2024-02-06 17:59:43 +0530

    Model Open And Update Case Run On Edit Button

--- application/controllers/manufacturing/Process.php
+++ application/controllers/manufacturing/Process.php
@@ -41,9 +41,6 @@ class Process extends CI_Controller
                    ->set_rules('given_qty','Given Quantity','required')
                    ->set_rules('given_weight','Given Weight','required')
                    ->set_rules('labour','Labour','required')
-                   ->set_rules('receive_qty','Received Quantity','required')
-                   ->set_rules('receive_weight','Received Weight','required')
-                   ->set_rules('total','Total','required')
                    ->set_error_delimiters('<div class="text-danger">', '</div>');
 
         if(!$validation->run()){
@@ -59,13 +56,44 @@ class Process extends CI_Controller
         $data['given_qty'] = $post['given_qty'];
         $data['given_weight'] = $post['given_weight'];
         $data['labour'] = $post['labour'];
-        $data['receive_qty'] = $post['receive_qty'];
-        $data['receive_weight'] = $post['receive_weight'];
-        $data['total'] = $post['total'];
         $data['creation_date'] = date('Y-m-d');
-        $data['created_at'] = date('Y-m-d H:i:s');
 
         $this->db->insert('given', $data);
         flash()->withSuccess("Garnu Added Successfully.")->back();
-    }   
+    }
+    
+    public function update()
+    {
+        $id = $this->input->post('given_id');
+
+        $validation = $this->form_validation;
+        $validation->set_rules('name','Garnu Name','required')
+                   ->set_rules('rc_qty','Received Quantity','required')
+                   ->set_rules('weight','Garnu Weight','required')
+                   ->set_rules('process','Process','required')
+                   ->set_rules('workers','Workers','required')
+                   ->set_rules('remarks','Remark','required')
+                   ->set_rules('given_qty','Given Quantity','required')
+                   ->set_rules('given_weight','Given Weight','required')
+                   ->set_rules('labour','Labour','required')
+                   ->set_error_delimiters('<div class="text-danger">', '</div>');
+
+        if(!$validation->run()){
+        return flash()->withError(validation_errors())->back();
+        }
+        $post = xss_clean($this->input->post());
+        $data = array();
+        $data['garnu_id'] = $post['garnu_id'];
+        $data['rc_qty'] = $post['rc_qty'];
+        $data['process'] = $post['process'];
+        $data['workers'] = $post['workers'];
+        $data['remarks'] = $post['remarks'];
+        $data['given_qty'] = $post['given_qty'];
+        $data['given_weight'] = $post['given_weight'];
+        $data['labour'] = $post['labour'];
+
+        $this->db->where('id',$id)
+                 ->update('given',$data);
+                 flash()->withSuccess("Update Successfully.")->back();
+    }
 }
